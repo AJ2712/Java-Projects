343f6da9cf136f5e311dd3f60ed6212fa8380567
Abhinandan <coolaj2712@gmail.com>
2023-04-21 05:14:33 +0530

    Initial Commit

---
+++ Pizza Bill Generator/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        pizza basepizza = new pizza(false);
+
+        basepizza.addExtraCheese();
+        basepizza.addExtraToppings();
+        basepizza.getbill();
+
+    }
+}
---
+++ Pizza Bill Generator/src/deluxPizza.java
@@ -0,0 +1,8 @@
+public class deluxPizza extends pizza{
+    public deluxPizza(Boolean veg ){
+        super(veg);
+        super.addExtraCheese();
+        super.addExtraToppings();
+
+    }
+}
