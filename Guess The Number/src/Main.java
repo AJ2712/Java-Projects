9f8f5a9caf1c73bb068f940ebe8c0641bb82ce78
Abhinandan <coolaj2712@gmail.com>
2023-04-24 17:24:03 +0530

    Initial Commit

---
+++ Guess The Number/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       Game g = new Game();
+       boolean b = false;
+       while(!b){
+           g.takeUserInput();
+           b = g.isCorrectNumber();
+       }
+
+    }
+}
